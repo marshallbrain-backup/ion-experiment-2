2aa82a4950f4407a5f7d0ada5bf6f258eabfb4e1
marshallbrain <marshalldbrain@gmail.com>
2020-01-19 14:54:44 -0500

    Initial commit

---
+++ src.jvm/src/test/kotlin/com/marshalldbrain/ion/collections/LinkedListTest.kt
@@ -0,0 +1,26 @@
+package com.marshalldbrain.ion.collections
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.FunSpec
+
+internal class LinkedListTest : FunSpec({
+	
+	test("Get size")
+	
+	test("add")
+	
+	test("peek")
+	
+	test("poll")
+	
+	context("Iterator Test") {
+		
+		test("Has next")
+		
+		test("Remove")
+		
+		test("Next")
+		
+	}
+	
+})
