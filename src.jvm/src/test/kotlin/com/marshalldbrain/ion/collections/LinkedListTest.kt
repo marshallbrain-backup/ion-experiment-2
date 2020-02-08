7f5f57bd4f67dd78f9b7c3eca7229b6941770691
marshallbrain <marshalldbrain@gmail.com>
2020-02-08 11:13:36 -0500

    Re formate test

--- src.jvm/src/test/kotlin/com/marshalldbrain/ion/collections/LinkedListTest.kt
+++ src.jvm/src/test/kotlin/com/marshalldbrain/ion/collections/LinkedListTest.kt
@@ -1,89 +1,133 @@
 package com.marshalldbrain.ion.collections
 
+import com.marshalldbrain.ion.sequences.randomIntRange
 import io.kotlintest.TestCase
 import io.kotlintest.matchers.boolean.shouldBeFalse
 import io.kotlintest.matchers.boolean.shouldBeTrue
+import io.kotlintest.matchers.collections.shouldBeEmpty
+import io.kotlintest.properties.Gen
+import io.kotlintest.properties.forAll
 import io.kotlintest.shouldBe
-import io.kotlintest.specs.AbstractAnnotationSpec
 import io.kotlintest.specs.FunSpec
+import kotlin.random.Random
 
-class LinkedListTest : FunSpec() {
+class LinkedListTest : FunSpec({
 	
-	private lateinit var linkedList: LinkedList<Any>
-	
-	override fun beforeTest(testCase: TestCase) {
+	test("Get size") {
 		
-		linkedList = LinkedList()
+		var linkedList: LinkedList<String>
+		
+		forAll(RangedInt(min = 1, max = 10)) { a: Int ->
+			
+			linkedList = LinkedList()
+			
+			forAll(a) { b: String ->
+				linkedList.add(b)
+			}
+			
+			linkedList.size == a
+			
+		}
 		
 	}
 	
-	init {
+	test("Add") {
 		
-		test("Get size") {
-			
-			linkedList.size shouldBe 0
-			
-		}
+		val linkedList = LinkedList<String>()
 		
-		test("Add") {
-			
-			linkedList.add("Test").shouldBeTrue()
-			
-		}
+		linkedList.add("Test").shouldBeTrue()
 		
-		test("Peek") {
+	}
+	
+	test("Peek") {
+		
+		val linkedList = LinkedList<String>()
+		
+		linkedList.add("Test")
+		
+		linkedList.peek() shouldBe "Test"
+		
+	}
+	
+	test("Poll") {
+		
+		val linkedList = LinkedList<String>()
+		
+		linkedList.add("Test")
+		
+		linkedList.poll() shouldBe "Test"
+		linkedList.size shouldBe 0
+		
+	}
+	
+	test("Adding to empty list") {
+		
+		val linkedList = LinkedList<String>()
+		linkedList.add("Test")
+		linkedList.poll()
+		
+		linkedList.shouldBeEmpty()
+		
+		linkedList.add("Test2")
+		
+		linkedList.peek() shouldBe "Test2"
+		linkedList.size shouldBe 1
+		
+	}
+	
+	context("Iterator Test") {
+		
+		test("Has next") {
+			
+			val linkedList = LinkedList<String>()
 			
 			linkedList.add("Test")
 			
-			linkedList.peek() shouldBe "Test"
+			val iterator = linkedList.iterator()
+			
+			iterator.hasNext().shouldBeTrue()
 			
 		}
 		
-		test("Poll") {
+		test("Remove").config(enabled = false) {
+			
+			val linkedList = LinkedList<String>()
 			
 			linkedList.add("Test")
 			
-			linkedList.poll() shouldBe "Test"
-			linkedList.size shouldBe 0
+			val iterator = linkedList.iterator()
+			
+			iterator.next()
+			iterator.remove()
+			
+			iterator.hasNext().shouldBeFalse()
 			
 		}
 		
-		context("Iterator Test") {
+		test("Next") {
 			
-			test("Has next") {
-				
-				linkedList.add("Test")
-				
-				val iterator = linkedList.iterator()
-				
-				iterator.hasNext().shouldBeTrue()
-				
-			}
+			val linkedList = LinkedList<String>()
 			
-			test("Remove").config(enabled = false) {
-
-				linkedList.add("Test")
-
-				val iterator = linkedList.iterator()
-
-				iterator.next()
-				iterator.remove()
-
-				iterator.hasNext().shouldBeFalse()
-
-			}
+			linkedList.add("Test")
 			
-			test("Next") {
-				
-				linkedList.add("Test")
-				
-				val iterator = linkedList.iterator()
-				
-				iterator.next() shouldBe "Test"
-				iterator.hasNext().shouldBeFalse()
-				
-			}
+			val iterator = linkedList.iterator()
 			
+			iterator.next() shouldBe "Test"
+			iterator.hasNext().shouldBeFalse()
+			
+		}
+		
+	}
+	
+}) {
+	
+	class RangedInt(val min: Int = 0, val max: Int) : Gen<Int> {
+		override fun constants(): Iterable<Int> {
+			return emptyList()
+		}
+		
+		override fun random(): Sequence<Int> {
+			return randomIntRange(min, max)
 		}
 		
 	}
